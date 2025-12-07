5e38cae0ee04a22e7c5a313ff008d3ad34cb720c
castlehyuuuun <152504934+castlehyuuuun@users.noreply.github.com>
2025-12-07 17:42:38 +0900

    [Bronze III] Title: 369, Time: 416 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,6 +1,6 @@
 n = int(input())
 count = 0
 for i in range(1, n+1):
-    if '3' in str(i) or '6' in str(i) or '9' in str(i):
-        count += 1
+    numFor3 = str(i)
+    count += numFor3.count('3') + numFor3.count('6') + numFor3.count('9')
 print(count)
